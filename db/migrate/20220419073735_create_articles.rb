9a7efa3712a8a9297028c04aae8d8048647141f5
youngjaw12 <agahjonathan@gmail.com>
2022-04-19 09:02:26 +0100

    added article scaffold and header

---
+++ db/migrate/20220419073735_create_articles.rb
@@ -0,0 +1,11 @@
+class CreateArticles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :content
+      t.string :auhor
+
+      t.timestamps
+    end
+  end
+end
